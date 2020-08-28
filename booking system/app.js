605dc4f2cadf79535bfbc7450a10a0459d554fb2
moonee <uuuu118@naver.com>
2020-08-28 23:17:48 +0900

    Booking System - 기초 클래스 구현완료 - 날짜 클릭시 해당 날짜 영화 리스트 띄워주기 완료

---
+++ booking system/app.js
@@ -0,0 +1,367 @@
+
+const $selectedContainer= document.getElementById('selected__list');
+const $reservationContainer=document.getElementById('reservation__container');
+const $screenContainer = document.getElementById('screening');
+const $date = document.querySelectorAll('.day__week');
+const $screenTitle=document.getElementById('screening__date');
+const screeningList=
+[
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+class Movie {
+    constructor(title, sequence, fee){
+        this.title=title;
+        this.sequence=sequence;
+        this.fee=fee;
+    }
+
+    getDate(){
+        return this.date;
+    }
+    getTitle(){
+        return this.title;
+    }
+
+    getSequence(){
+        return this.sequence;
+    }
+    getFee(){
+        return this.fee;
+    }
+}
+
+
+class Money{
+    constructor(total, discount){
+        this.total=total;
+        this.discount=discount;
+    }
+
+    getTotal(){
+        return this.total;
+    }
+
+    getDiscount(){
+        return this.discount;
+    }
+
+    setTotal(money){
+        this.total=money;
+    }
+
+    setDiscount(discount){
+        this.discount=discount;
+    }
+
+    getMovieFee(){
+        return this.total-this.discount;
+    }
+}
+
+class Screening{
+    constructor(date,movie, startTime, discountPolicy){
+        this.date=date;
+        this.movie=movie;
+        this.startTime=startTime; // 시작시간
+        this.discountPolicy=discountPolicy;
+        this.endTime=(+movie.getSequence())+startTime;
+    }
+    
+    getTime(){ // 상영시간 
+        // start time~ endtime 조합해서 리턴하기
+        let start= +this.startTime;
+        let end = +this.endTime;
+        if(start>=24){
+            start-=24;
+        }
+        if(end >=24){
+            end-=24;
+        }
+        start.toString().replace('.',':');
+        end.toString().replace('.',':');
+        return `${start}~${end}`;
+    }
+    
+    getMovieTitle(){
+        return this.movie.getTitle();
+    }
+
+    
+    CalcMoney(){ // 할인가 계산 
+        const movieFee=this.movie.getFee();
+        const requirement = this.discountPolicy.getRequirement();
+        const money = new Money(movieFee,0);
+        if(requirement.check(this.startTime, this.endTime)===true){ // 할인 조건 충족시 할인 
+            const discount = this.discountPolicy.getDiscount();
+            if(discount < 0){ // static 
+                money.setDiscount(discount*(-1));
+            }
+            else{ // percentage 
+                money.setDiscount(movieFee*discount);
+            }
+         }
+        return money;
+    }
+}
+
+class Reservation{
+    constructor(screening, money){
+        this.screening=screening;
+        this.money=money;
+    }
+    
+    getScreening (){
+        return this.screening;
+    }
+
+    getMoney(){
+        return this.money;
+    }
+}
+
+class DiscountPolicy{ // 할인 정책 
+    constructor(discountRequirment,amount){
+        this.discountRequirment=discountRequirment;  // 할인 조건 
+        this.amount=amount;
+    }
+    
+    getRequirement(){
+        return this.discountRequirment;
+    }
+    getDiscount(){
+        return this.amount;
+    }
+}
+
+class StaticDiscount extends DiscountPolicy{ // 특정 금액 할인 
+    constructor(discountRequirment, amount){
+        super(discountRequirment,amount);
+    }
+    getDiscount(){
+        return this.amount*(-1);
+    }
+}
+
+class PercentageDiscount extends DiscountPolicy{ // 퍼센테이지 할인 
+    constructor(discountRequirment, amount){
+        super(discountRequirment,amount);
+    }
+    getDiscount(){
+        return this.amount/100; 
+    }
+}
+
+class DiscountRequirement{ // 특정 상영시간 내의 영화만 할인 
+    constructor(startTime, endTime){
+        this.startTime=startTime;
+        this.endTime=endTime;
+    }
+    
+    check(startTime,endTime){
+        if(startTime>=this.startTime || endTime <= this.endTime){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+}
+
+// 저장될 것
+// 선택된 영화 리스트
+// 예약한 영화 리스트 
+// 상영 영화 리스트 
+class Store{ // local Storage에 저장하는 클래스 
+
+    getSelectedList(){
+        return JSON.parse(localStorage.getItem('selectedList'))||[];
+    }
+
+    getReservedList(){
+        return JSON.parse(localStorage.getItem('reservedList'))||[];
+    }
+
+
+    setSelectedList(newItem){
+        const selectedList=this.getSelectedList();
+        selectedList.push(newItem);
+        localStorage.setItem('selectedList',JSON.stringify(selectedList));
+    }
+
+    setReservedList(newItem){
+        const reservedList = this.getReservedList();
+        reservedList.push(newItem);
+        localStorage.setItem('reservedList',JSON.stringify(reservedList));
+    }
+
+}
+
+class CalcDate{
+    constructor(){
+        const today= new Date();
+        this.date= today.getDate();
+        this.month= today.getMonth();
+        this.year= today.getFullYear();
+    }
+
+    getDate(){
+        return this.date;
+    }
+
+    getMonth(){
+        return this.month;
+    }
+
+    getYear(){
+        return this.year;
+    }
+    
+    getDaysInMonth(){
+        return new Date(parseInt(this.year), parseInt(this.month)+1,0).getDate();
+    }
+
+}
+
+const date = new CalcDate();
+
+const store = new Store();
+
+class UI{
+
+    initSelectedContainer(){
+        if(store.getSelectedList().length===0){
+            $selectedContainer.classList.add('hidden');
+        }
+    }
+
+    initReservedContainer(){
+        if(store.getReservedList().length===0){
+            $reservationContainer.classList.add('hidden');
+        }
+    }
+
+    showMovies(selectedDate){ // 특정 날짜 선택시, 해당 날짜에 상영하는 영화 나열 
+        const daysInMonth = +date.getDaysInMonth(); 
+        const today = +date.getDate();
+        let idx;
+        for(let i = 0; i<7; i++){
+            let newDate= today+i;
+            if(newDate>daysInMonth){
+                newDate-=daysInMonth;
+            }
+            if(newDate === selectedDate){
+                idx=i;
+                break;
+            }
+        }
+       const screenList= screeningList[idx];  // 해당 날짜에 상영하는 영화 리스트
+       let html =`
+       <tr>
+            <th>상영시간</th>
+            <th>제목</th>
+            <th>가격</th>
+        </tr>
+        `
+       screenList.forEach(movie=>{
+           console.log(movie);
+           const money= movie.CalcMoney();
+           const totalFee = money.getTotal()-money.getDiscount();
+           html+=`
+           <tr>
+                <td>${movie.getTime()}</td>
+                <td>${movie.getMovieTitle()}</td>
+                <td>${totalFee}</td>
+
+           </tr>
+           `;
+       })
+       $screenContainer.innerHTML=html;
+    }
+}
+
+
+// 조조 할인
+const morningDiscount = new DiscountRequirement(7,12);
+// 심야 할인 
+const nightDiscount = new DiscountRequirement(22,24);
+
+// 할인 정책 
+const policy1=  new StaticDiscount(morningDiscount,3000); // 조조할인 + 고정가격할인
+const policy2= new StaticDiscount(nightDiscount,2000); // 심야할인 + 고정가격 할인
+const policy3= new PercentageDiscount(morningDiscount, 20); // 조조할인 + 퍼센테이지 할인
+const policy4 = new PercentageDiscount(nightDiscount, 10); // 심야할인 + 퍼센테이지 할인
+
+
+// 상영 스케줄 로컬 스토리지에 저장   
+// 점심에는 기본적으로 할인정책 들어가지만, 적용되지는 않음
+const initScreening=function(){ 
+    // 영화 리스트 
+    const movie1 = new Movie('작은 아씨들', '2', '12000');
+    const movie2 = new Movie('브루클린', '1.30','12000');
+    const movie3 = new Movie('다크나이트', '3', '11000');
+    const movie4 = new Movie('인셉션', '2.30', '12000');
+    const movie5 = new Movie('센과 치히로의 행방불명', '2', '8000');
+    const movie6 = new Movie('가디언즈 오브 갤럭시', '3', '10000');
+    const movie7 = new Movie('어느 가족', '2', '11000');
+
+    const today_date=date.getDate();
+    const DaysInMonth= date.getDaysInMonth();
+    for(i=0;i<7;i++){ 
+        let newDate=today_date+i;
+        if(newDate>=DaysInMonth){
+            newDate-=DaysInMonth;
+        }
+        let obj=new Screening(newDate,movie1,7,policy1);
+        screeningList[i].push(obj);
+        screeningList[i].push(new Screening(newDate,movie2,8,policy3));
+        screeningList[i].push(new Screening(newDate,movie4,7.30,policy1));
+        screeningList[i].push(new Screening(newDate,movie5,6,policy3));
+
+        screeningList[i].push(new Screening(newDate,movie6,12,policy1));
+        screeningList[i].push(new Screening(newDate,movie1,12,policy1));
+        screeningList[i].push(new Screening(newDate,movie3,13,policy1));
+        screeningList[i].push(new Screening(newDate,movie5,15,policy3));
+
+        screeningList[i].push(new Screening(newDate,movie1,17,policy1));
+        screeningList[i].push(new Screening(newDate,movie5,18,policy1));
+        screeningList[i].push(new Screening(newDate,movie3,21,policy1));
+        screeningList[i].push(new Screening(newDate,movie2,20,policy3));
+
+        screeningList[i].push(new Screening(newDate,movie7,22,policy1));
+        screeningList[i].push(new Screening(newDate,movie7,23,policy1));
+        screeningList[i].push(new Screening(newDate,movie2,21.30,policy1));
+        screeningList[i].push(new Screening(newDate,movie6,24,policy3));
+    }
+
+}
+
+
+const ui = new UI();
+const init=function(){
+    ui.initSelectedContainer();
+    ui.initReservedContainer();
+}
+const dateEvent=function(){
+    $date.forEach(date=>{
+        date.addEventListener('click',(e)=>{
+            // 일 , 월 만 빼오기  
+            
+            const selectedDate=date.children[1].innerText;
+            const selectedMonth=date.children[0].innerText;
+            $screenTitle.innerText=`${selectedMonth} ${selectedDate}`;
+            ui.showMovies(+selectedDate.replace('일',''));
+        })
+    })
+}
+init();
+
+dateEvent();
+
+initScreening();
---
+++ booking system/calendar.js
@@ -0,0 +1,50 @@
+// 오늘부터 일주일 구현하기 
+const $weekly=document.getElementById('weekly__calendar');
+
+
+
+const getWeek= function(params){
+    const month=['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월', '11월', '12월'];
+    const day = ['월','화','수','목','금','토','일'];
+    const daysInMonth=getDaysInMonth(params.month, params.year);
+    console.log(daysInMonth);
+    function getDaysInMonth(month,year){ // 현재 month의 총 일자 구하기 
+        return new Date(year, month+1,0).getDate();
+    }
+    let html='';
+    let newMonth = month[params.month]; // 월 
+    let dayCount = params.day-2; // 요일 
+
+    for(let i=0; i<7; i++){ //일주일단위
+        dayCount++;
+        if(dayCount === 7) {
+            dayCount=0;
+        }
+        let newDay = day[dayCount];
+        let newDate = params.date+i;
+        if(newDate > daysInMonth) {
+            newDate-=daysInMonth;
+            params.month ===11 ? newMonth=month[0] : newMonth=month[params.month+1]; // month 도 바꿔주기
+        }
+        html+=`<div class="day__week ${i===0 ? 'today' :''}">
+            <span id="Month">${newMonth}</span>
+            <span id="Date">${newDate}일</span>
+            <span id="Day">${newDay}</span> 
+        </div>`;
+    }
+    $weekly.innerHTML=html;
+}
+
+
+const today= new Date();
+const now_date = today.getDate();
+const now_month = today.getMonth();
+const now_day = today.getDay();
+const now_year = today.getFullYear();
+
+getWeek({
+    month: now_month,
+    date: now_date,
+    day: now_day,
+    year: now_year
+});
